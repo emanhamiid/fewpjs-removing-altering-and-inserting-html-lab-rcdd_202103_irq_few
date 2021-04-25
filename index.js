0fbaf65a4c7999678b619e823bebb667e5be01f9
eman hamid <emanhharki@gmail.com>
2021-04-25 14:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 main.remove();
 let newHeader = document.createElement("h1");
 newHeader.setAttribute("id","victory");
+<<<<<<< HEAD
 newHeader.innerHTML=("eman is the champion");
 // newHeader.innerText("eman is the champion");
 // newHeader.setAttribute("id","victory");
+=======
+newHeader.innerText("eman");
+>>>>>>> 75659f38daf09911e61a2f27962b4ffccf22b679
